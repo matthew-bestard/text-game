0a6b99da020db3db99706f31bb38e3701f241e2b
Matthew Bestard <matthew.r.bestard@gmail.com>
2018-04-26 15:26:28 -0400

    include ncurses header(after download of ncurses library), change game.c to a basic ncurses hello world program, update make file to include -lncurses option

--- game.c
+++ game.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <ncurses.h>
 
 int main (void) {
-  printf("this is a test output");
+  initscr(); // initializes the screen, sets up memory and clears the screen
+  printw("Hello World !!!");
+  refresh(); // refreshes screen to match what is in memory
+
+  int c = getch();
+
+  printw("%d", c);
+
+  getch(); // Gets user input and returns int value of the key typed in
+  endwin(); // Deallocates memory and ends ncurses 
   return 0;
 }
